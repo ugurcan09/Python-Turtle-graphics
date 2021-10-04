931c64166080eaa3db5a8d6faa94be2441793d4e
ugurcan09 <ugurcan2953@gmail.com>
2021-10-04 19:53:04 +0300

    Create Hexagon-design.py

---
+++ Hexagon-Design/Hexagon-design.py
@@ -0,0 +1,20 @@
+import turtle
+
+col=('red','yellow','green','cyan','pink',
+'white')
+
+t=turtle.Turtle()
+turtle.title("Hexagon Design")
+
+screen=turtle.Screen()
+screen.bgcolor('black')
+t.speed(25)
+
+for i in range (150):
+
+    t.color(col[i%6])
+    t.forward(i*1.5)
+    t.left(59)
+    t.width(3)
+
+turtle.end_fill()
